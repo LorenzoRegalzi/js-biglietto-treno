218233f7d8714d87f18c6d0852739013c1d01e02
l <lorenzo.regalzi@gmail.com>
2020-02-18 18:46:54 +0100

    esercizio finito

--- js/main.js
+++ js/main.js
@@ -8,3 +8,11 @@ console.log(età);
 var calcolo= km * 0.21;
 
 console.log(calcolo + '$ è il prezzo senza sconto');
+
+if (età<18) {
+    console.log((Math.round(calcolo - ((calcolo*20)/100)))+ '$ è il prezzo scontato under 18');
+} else if (età>=18 && età<=63) {
+    console.log((Math.round(calcolo)) + '$ è il prezzo non scontato');
+} else if (età>63) {
+    console.log((Math.round(calcolo - ((calcolo*40)/100)))+ '$ è il prezzo scontato over 64');
+}

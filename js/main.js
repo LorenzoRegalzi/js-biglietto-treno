cf12355226ba146a0838a1f4a66496b474450152
l <lorenzo.regalzi@gmail.com>
2020-02-18 15:11:29 +0100

    primo calcolo

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+var km= prompt('quanti km devi percorrere?', '');
+var età= prompt('quanti anni hai?', '');
+
+console.log(km);
+console.log(età);
+
+var calcolo= km * 0.21;
+
+console.log(calcolo + '$ è il prezzo senza sconto');
